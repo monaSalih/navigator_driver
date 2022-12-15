c8da6e6c51e4f241c86510fe4600b476f7ba79a1
unknown <talab.yaseen@gmail.com>
2022-12-15 11:08:10 +0300

    second commit

--- newtest.php
+++ newtest.php
@@ -1,3 +1,16 @@
 <?php
 echo "new file";
+class human {
+    function __construct($arr)
+    {
+        foreach ($arr as $key => $val){
+            if ($key%$val == 0){
+                echo true . "<br>";
+            }else {
+                echo false . "<br>";
+            }
+        }
+    }
+};
+
 ?>
